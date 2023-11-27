bbe8329c04f2892c7a3ad6117290f44d843b60db
davickreations <davickreations2@gmail.com>
2023-11-27 16:20:50 +0600

    health blog

---
+++ README.md
@@ -0,0 +1,22 @@
+# HealthHoot Fitness Website
+
+Welcome to the HealthHoot Fitness Website repository! This project is a responsive and dynamic website designed for health and fitness enthusiasts. It includes features such as informative blog posts, personalized fitness plans, and a supportive community section.
+
+## Key Features
+
+- Blog section with insightful articles on fitness, nutrition, and wellness.
+- Dynamic sliders showcasing upcoming events, fitness classes, and expert consultations.
+- Personalized fitness plans and nutrition guidance for users.
+- Interactive community features, providing a platform for members to connect and share experiences.
+
+## Technologies Used
+
+- HTML5
+- CSS3
+- JavaScript
+- Swiper.js (for sliders)
+
+Feel free to explore the code, contribute, or use this project as a template for your fitness or health-related website.
+
+Happy coding and stay fit!
+
---
+++ script.js
@@ -0,0 +1,127 @@
+
+// for header
+
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+//for window scroll
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+
+    if (window.scrollY > 0) {
+        document.querySelector('.header').classList.add('active');
+    } else {
+        document.querySelector('.header').classList.remove('active');
+    }
+
+};
+
+window.onload = () => {
+    if (window.scrollY > 0) {
+        document.querySelector('.header').classList.add('active');
+    } else {
+        document.querySelector('.header').classList.remove('active');
+    }
+}
+
+
+
+
+//for home slider
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    effect: "fade",
+    grabCursor: true,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+      },
+  });
+
+
+  //for feature slider
+
+var swiper = new Swiper(".feature-slider", {
+    spaceBetween: 30,
+    grabCursor: true,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+      },
+
+      breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+      },
+  });
+
+
+    //for trainer slider
+
+var swiper = new Swiper(".trainer-slider", {
+    spaceBetween: 30,
+    grabCursor: true,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+      },
+
+      breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+      },
+  });
+
+
+      //for blog slider
+
+var swiper = new Swiper(".blog-slider", {
+    spaceBetween: 30,
+    grabCursor: true,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 8500,
+        disableOnInteraction: false,
+      },
+
+      breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+      },
+  });
